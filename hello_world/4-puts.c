1dafdc1880ac3c05da98c712deff9546d79eb9dd
root <serifekaragur@gmail.com>
2025-02-17 13:57:43 +0000

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-/* 
- * main -  print a message
- * return to 0
+/** 
+ *  main -  print a message 
+ *  
+ *  Return : 1 if the nÄumber is positive, 0 otherwise 
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
