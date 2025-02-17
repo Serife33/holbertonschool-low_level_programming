230468b55b923671088198edd8dfe716df9303e6
root <serifekaragur@gmail.com>
2025-02-17 14:41:25 +0000

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+ */
+int main(void)
+{
+        printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
