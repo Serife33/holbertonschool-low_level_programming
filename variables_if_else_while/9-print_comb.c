78cd201635b2ebf936a5156b0919833f27158d6f
root <serifekaragur@gmail.com>
2025-02-19 10:44:27 +0000

    numbers separate

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,13 +6,16 @@
  */
 int main(void)
 {
-	int i = '0';
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
