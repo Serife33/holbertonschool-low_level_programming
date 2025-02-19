b2abd448ff517ea0d4f3056113acd40945f64bd3
root <serifekaragur@gmail.com>
2025-02-19 08:58:18 +0000

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ * Return: turns 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
