27230be4332a2fd6bd5604b6a659b045a3d4e345
root <serifekaragur@gmail.com>
2025-02-20 14:54:25 +0000

    alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet_x10  - alphabet with _putchar 10 times
+ *
+ * Return: turns 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 0; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+	_putchar('\n');
+	}
+}
