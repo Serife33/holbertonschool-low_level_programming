28ae89a34d2cbe5a54b84c2da33eacd0c781eadc
root <serifekaragur@gmail.com>
2025-02-24 13:40:03 +0000

    multiply 2 numbers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiply number.
+ *
+ * @a: the character to multiply
+ *
+ * @b: the character to multiply
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
