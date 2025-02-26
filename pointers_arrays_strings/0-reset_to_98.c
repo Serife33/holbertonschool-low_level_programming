aa3c7d926965d6bbc588fa15001f70da62d81009
root <serifekaragur@gmail.com>
2025-02-26 09:13:57 +0000

    reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - dereference pointer
+ *
+ * @n: the parameter
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
