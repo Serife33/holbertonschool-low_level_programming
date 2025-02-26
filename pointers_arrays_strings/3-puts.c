b505a1a081e5f7efa79c2e4954fd384ce5e9a1f2
root <serifekaragur@gmail.com>
2025-02-26 10:45:23 +0000

    print a string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - put the words 
+ *
+ * @str: character to print
+ *
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	char *p = str;
+
+	while (*p != '\0')
+	{
+		_putchar(*p);
+		p++;
+	}
+	_putchar('\n');
+}
